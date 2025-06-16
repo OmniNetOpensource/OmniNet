f69e6c9796eaf051fc32f910c4717677d0bc793e
OmniNetOpensource <opensource@omninet.space>
2025-06-16 23:17:59 +0800

    fix paragraph text as string

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import './styles/app.css'
 // 常量
 const WORDS_LINE1 = "we love making";
 const WORDS_LINE2 = "cool stuffs";
-const PARAGRAPH = "好的念頭 是一切的開端 我們樂於動手將它實現 \n 不論這想法是自己苦思的 還是從眾人那裡得來的 每個念頭皆有其價值 \n 有時它清晰明確 有時仍有些模糊".split('');
+// 多行段落文本
+const PARAGRAPH =
+  "好的念頭 是一切的開端 我們樂於動手將它實現 \n 不論這想法是自己苦思的 還是從眾人那裡得來的 每個念頭皆有其價值 \n 有時它清晰明確 有時仍有些模糊";
 
 
 export default function App() {
